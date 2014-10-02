43b75ce740c0f7c5e79c7630f30e3ed08965e8c8
Sn0w <garrettbeasley@icloud.com>
2014-10-01 19:05:13 -0700

    Init Commit

---
+++ src/PP_Sales_Applet.java
@@ -0,0 +1,29 @@
+/**
+ * PP_Sales_Applet.java
+ * @author Garrett J. Beasley
+ * 10/03/2014
+ * Draws a House Picture
+ */
+
+//Imports for the program.
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import java.awt.Color;
+import java.util.Random;
+
+public class PP_Sales_Applet extends GraphicsProgram 
+{
+	public void run()
+	{
+		
+		//Variables for the project.
+		/**Values for the window Size*/
+		int windowX = 400;
+		int windowY = 300;
+		
+		//Sets the size of the window for the program
+		 setSize(windowX, windowY);
+		 
+		 
+	}
+}
